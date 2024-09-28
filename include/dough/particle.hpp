bd08ecc25ee83f93c4755f4dfc6c6b2c539c1bcb
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-09-28 10:32:25 -0400

    set and get

--- include/dough/particle.hpp
+++ include/dough/particle.hpp
@@ -13,13 +13,33 @@ namespace dough {
         : position(position), velocity(velocity), damping(damping) {
             setMass(Mass);
         }
+        void setPosition(Vector3 _position) {
+            position = _position;
+        }
+        void setVelocity(Vector3 _velocity) {
+            velocity = _velocity;
+        }
+        void setAcceleration(Vector3 _acceleration) {
+            acceleration = _acceleration;
+        }
+        void setDamping(real _damping) {
+            damping = _damping;
+        }
         void setMass(real Mass) {
             inverseMass = (real)1/Mass;
         }
-        void integrate(real time);
         
         Vector3 getPosition() {
             return position;
         }
+        Vector3 getVeloicty() {
+            return velocity;
+        }
+
+        void clearAcceleration() {
+            acceleration = Vector3(0,0,0);
+        }
+
+        void integrate(real time);
     };
 }
--- main.cpp
+++ main.cpp
@@ -5,26 +5,28 @@
 Camera camera = {0};
 
 int main() {
-    dough::Particle particle = dough::Particle(dough::Vector3(1,1,1), dough::Vector3(0.2,0.5,0.1), 0.999, 3);
+    dough::Particle particle = dough::Particle(dough::Vector3(0,0,0), dough::Vector3(0.0,2.0,-2.0), 0.999, 3);
 
     SetTargetFPS(60);
     InitWindow(800, 800, "test");
 
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
+    camera.position = (Vector3){ 10.0f, 0.0f, 0.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 60.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
     while(!WindowShouldClose()) {
-        UpdateCamera(&camera, CAMERA_ORBITAL);
+        UpdateCamera(&camera, CAMERA_PERSPECTIVE);
+        particle.setAcceleration(dough::Vector3(0,-2,0));
         particle.integrate((float)1/60);
+        particle.clearAcceleration();
         Vector3 position = Vector3{particle.getPosition().x, particle.getPosition().y, particle.getPosition().z};
         BeginDrawing();
             BeginMode3D(camera);
 
             ClearBackground(BLACK);
-            DrawSphere(position, 1, WHITE);
+            DrawSphere(position, 0.5, WHITE);
             EndMode3D();
         EndDrawing();
     }
