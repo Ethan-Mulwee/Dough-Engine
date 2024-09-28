acf0762fb00ce607b387ba8262bb1f2275451ec6
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-09-28 10:07:40 -0400

    Basic visual

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "dough/particle.hpp"
+#include <raylib.h>
 
-using namespace dough;
+
 int main() {
-    Particle particle = Particle(Vector3(1,1,1), Vector3(2,5,0), 0.999, 3);
-    std::cout << particle.getPosition().x << std::endl;
-    particle.integrate(1);
-    std::cout << particle.getPosition().x << std::endl;
+    dough::Particle particle = dough::Particle(dough::Vector3(1,1,1), dough::Vector3(2,5,0), 0.999, 3);
+    SetTargetFPS(60);
+    InitWindow(800, 800, "test");
+    while(!WindowShouldClose()) {
+        //std::cout << particle.getPosition().x << std::endl;
+        particle.integrate(0.1);
+        Vector3 position = Vector3{particle.getPosition().x, particle.getPosition().y, particle.getPosition().z};
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawCircle(position.x, position.y, 6, WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
 }
