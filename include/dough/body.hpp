dce5737110ad8911e217ae8ea5c5cfc33bee4d98
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-10-18 08:56:26 -0400

    Added some data and methods to rigidbody class

--- include/dough/body.hpp
+++ include/dough/body.hpp
@@ -5,10 +5,25 @@
 namespace dough {
     class RigidBody {
         private:
+        Vector3 position;
+        Quaternion orientation;
+        real angularDamping;
+        real linearDamping;
         Matrix3 inverseInertiaTensor;
+
+        Vector3 forceAccum;
+        Vector3 torqueAccum;
+
+        bool isAwake;
 
         public:
         void setInteriaTensor(const Matrix3 &interiaTensor);
+
+        void integrate(real time);
+
+        void clearAccumlators();
+
+        void addForce(const Vector3 &force);
     };
 }
 
